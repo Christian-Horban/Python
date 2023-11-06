9455096686975bff21d528516c1c529baf8ec715
Git-Xian831 <christian.k.horban@gmail.com>
2023-11-06 16:58:17 -0700

    1.1

---
+++ add.py
@@ -0,0 +1,11 @@
+# Prompt user for the first number, convert it to an integer, and store it in variable a
+a = int(input("Enter the first number: "))
+
+# Prompt user for the second number, convert it to an integer, and store it in variable b
+b = int(input("Enter the second number: "))
+
+# Add variables a and b and store the result in variable c
+c = a + b
+
+# Print the value of c
+print("The sum of the two numbers is:", c)
